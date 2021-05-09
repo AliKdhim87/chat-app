cb1486d7e1dd2004895f663a520912fbf0921256
ali <a.khadim@xpay.de>
2021-05-09 16:31:20 +0200

    Migrating from 2x to 4.0 socket.io

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const cors = require("cors");
-const formatMessage = require("./utils/messages");
+// @ts-check
+const express = require("express")
+const { Server } = require("socket.io")
+const http = require("http")
+
+const common = require("./utils/common")
+const config = common.config()
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: config.CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+})
+
+const formatMessage = require("./utils/messages")
 const {
   addUser,
   removeUser,
   getUser,
   getUsersInRoom,
-} = require("./utils/users");
+} = require("./utils/users")
 
-const PORT = process.env.PORT || 5000;
-app.use(cors());
+const PORT = process.env.PORT || 5000
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected")
 
   socket.on("join", ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+    const { error, user } = addUser({ id: socket.id, name, room })
 
-    if (error) return callback(error);
+    if (error) return callback(error)
 
-    socket.join(user.room);
+    socket.join(user.room)
 
     socket.emit(
       "message",
       formatMessage("Owner", `${user.name}, welcome to room ${user.room}.`)
-    );
+    )
     socket.broadcast
       .to(user.room)
-      .emit("message", formatMessage("Owner", `${user.name} has joined!`));
+      .emit("message", formatMessage("Owner", `${user.name} has joined!`))
 
     io.to(user.room).emit("onlineUsers", {
       room: user.room,
       users: getUsersInRoom(user.room),
-    });
+    })
 
-    callback();
-  });
+    callback()
+  })
 
   socket.on("sendMessage", (msg) => {
-    const user = getUser(socket.id);
+    const user = getUser(socket.id)
 
-    io.to(user.room).emit("message", formatMessage(user.name, msg));
-  });
+    io.to(user.room).emit("message", formatMessage(user.name, msg))
+  })
   socket.on("disconnect", () => {
-    console.log("User disconnected");
-    const user = removeUser(socket.id);
+    console.log("User disconnected")
+    const user = removeUser(socket.id)
     if (user) {
       io.to(user.room).emit(
         "message",
         formatMessage("Owner", `${user.name} has left.`)
-      );
+      )
       io.to(user.room).emit("onlineUsers", {
         room: user.room,
         users: getUsersInRoom(user.room),
-      });
+      })
     }
-  });
-});
+  })
+})
 
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
